74604ee04302a35738b635a5dc2c4f9bb654d1ca
AmaroBMZ <am.barria@duocuc.cl>
2026-03-25 11:27:09 -0300

    path añadido

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bibliotecaduoc.model.Libro;
 import com.example.bibliotecaduoc.service.LibroService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -34,6 +37,12 @@ public class LibroController {
         //TODO: process POST request
         libroService.save(libro);
         return libro;
+    }
+    
+
+    @GetMapping("{id}")
+    public Libro buscaLibro(@PathVariable int id) {
+        return libroService.readId(id);
     }
     
 }
